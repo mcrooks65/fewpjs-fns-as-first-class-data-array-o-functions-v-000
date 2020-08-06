6b28f3a79831fb4b697c6b9b9f3483b068a6ca1c
Matthew Crooks <mfcrooks@gmail.com>
2020-08-06 00:59:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,5 @@ function unleashDog(dogName, dogBreed) {
   console.log (`Unleash ${dogName} the ${dogBreed}`)
   return `Unleash ${dogName} the ${dogBreed}`
 }
+
+const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
