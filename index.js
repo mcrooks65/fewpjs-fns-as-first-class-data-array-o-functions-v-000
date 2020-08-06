493cfc3915d24959edd354faf037f40a79be0a02
Matthew Crooks <mfcrooks@gmail.com>
2020-08-06 00:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`)
   return `Walk to the park with ${dogName} the ${dogBreed}`
 }
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`)
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`
+}
